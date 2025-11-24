c53aa5570b4c26d9c566228888bf3cc66cac354b
Sumanth Reddy Busireddy <nanireddy9777@gmail.com>
2025-11-24 13:33:03 -0500

    Fix: Final verification of aws-static module files (variables.tf and outputs.tf).

--- modules/aws-static/outputs.tf
+++
@@ -1,9 +0,0 @@
-# modules/aws-static/outputs.tf
-
-# This output exports the static website endpoint URL, which is used by the
-# root module and the CI/CD pipeline's smoke test.
-output "website_url" {
-  description = "The public endpoint of the static website."
-  # The value is derived from the S3 bucket's website configuration resource.
-  value       = aws_s3_bucket_website_configuration.website_config.website_endpoint
-}
