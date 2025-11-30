e288ef3c68b6f32fd6412b33e2d3cc57c6eaca82
Sumanth Reddy Busireddy <nanireddy9777@gmail.com>
2025-11-30 18:42:23 -0500

    static azure fix

--- modules/azure-static/main.tf
+++ modules/azure-static/main.tf
@@ -53,7 +53,7 @@ resource "null_resource" "upload" {
   }
 
   provisioner "local-exec" {
-    command = "az storage blob upload-batch -s ${local.content_dir} -d '$web' --account-name ${azurerm_storage_account.sa.name} --auth-mode key"
+    command = "az storage blob upload-batch -s ${local.content_dir} -d '$web' --account-name ${azurerm_storage_account.sa.name} --auth-mode login"
   }
 }
 
